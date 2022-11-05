4d7d0dba8d8f95a1bdfe5b3cd888c976c393da60
lloydzhou <lloydzhou@qq.com>
2022-11-06 00:50:13 +0800

    init

---
+++ src/lib/Chart.tsx
@@ -0,0 +1,150 @@
+import { FC, createContext, useContext, createRef, useCallback, useEffect, useRef } from 'react';
+import {ECharts, EChartsCoreOption, init as initChart, throttle } from 'echarts/core'
+import { addListener, removeListener } from "resize-detector";
+
+export const ChartContext = createContext({})
+
+interface ChartProps {
+  onRendered: (chart: ECharts) => any
+  onFinished: (chart: ECharts) => any
+  width: number | "auto" | undefined
+  height: number | "auto" | undefined
+  autoResize: boolean
+  theme: string | object
+  notMerge: boolean
+  lazyUpdate: boolean
+}
+
+export const Chart: FC<Partial<EChartsCoreOption & ChartProps>> = (props) => {
+
+  const  {
+    children, className='', style={},
+    onRendered, onFinished,
+    width=600, height=300,
+    autoResize, theme,
+    notMerge, lazyUpdate, group,
+    ...other
+  } = props
+
+  const container = createRef<HTMLDivElement>()
+
+  const options = useRef<EChartsCoreOption>(other)
+  const replaceMerge = useRef(new Set())
+  const chart = useRef<ECharts | null>()
+
+  const setOption = (key: string, option: any) => {
+    console.log('setOption', key, option)
+    // 1. 移除option中无用的key
+    Object.keys(option).forEach(name => {
+      if (option[name] === undefined || option[name] === null) {
+        delete option[name]
+      }
+    })
+    // 2. 往当前的state.options中合并新的组件配置
+    if (!options.current[key]) {
+      options.current[key] = []
+    }
+    // 尝试先移除一下相同的id，避免出现相同的id配置项
+    removeOption(key, option.id)
+    // @ts-ignore
+    options.current[key].push(option)
+    // 3. 增加replaceMerge
+    replaceMerge.current.add(key)
+    // 4. 提交更新到echarts
+    commit()
+  }
+
+  const removeOption = (key: string, id: string) => {
+    if (options.current[key]) {
+      // 1. 移除组件配置
+      // @ts-ignore
+      options.current = { ...options.current, [key]: options.current[key].filter((i: any) => i.id !== id)}
+      // 2. 增加replaceMerge
+      replaceMerge.current.add(key)
+      // 3. 提交更新到echarts
+      commit()
+    }
+  }
+
+  // 这里实际上形成了一个批量更改一次提交的过程
+  // 避免了一个子组件有变化（例如series中的数据）
+  // 提交的时候影响到另一个子组件（例如xAxis）
+  const commit = throttle(useCallback(() => {
+    console.log('chart', chart, options)
+    if (chart.current) {
+      // 提交画布更新的时候，使用replaceMerge选项
+      // @ts-ignore
+      chart.current.setOption(options.current, {
+        lazyUpdate,
+        replaceMerge: Array.from(replaceMerge.current),
+      })
+      // 提交画布更新之后，重置replaceMerge
+      replaceMerge.current = new Set()
+    }
+  }, [lazyUpdate]), 50, true)
+
+  // @ts-ignore
+  const rendered = () => onRendered && onRendered(chart)
+  // @ts-ignore
+  const finished = () => onFinished && onFinished(chart)
+
+  const init = useCallback(() => {
+    const instance = initChart(container.current as HTMLDivElement, theme as string | object)
+    if (group) {
+      instance.group = group as string
+    }
+    instance.on('rendered', rendered)
+    instance.on('finished', finished)
+    console.log('init chart', instance)
+    chart.current = instance
+    // 使用默认的option初始化画布
+    commit()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    init()
+  }, [init])
+
+  // watch size
+  useEffect(() => {
+    if (chart.current && width && height) {
+      chart.current.resize({ width, height })
+    }
+  }, [width, height, chart])
+
+  // autoresize
+  useEffect(() => {
+    const resizeListener = throttle(() => {
+      chart.current && chart.current.resize()
+    }, 100)
+    if (container.current && chart.current && autoResize) {
+      addListener(container.current, resizeListener)
+    }
+    return () => {
+      if (container.current) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        removeListener(container.current, resizeListener)
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [autoResize, chart, container.current])
+
+  return (
+    <ChartContext.Provider value={{options, chart: chart.current, setOption, removeOption}}>
+      <div ref={container} className={`echarts ${className}`} style={{
+        display: 'block',
+        width: '100%',
+        height: '100%',
+        ...(style as object),
+      }}>
+        {children as any}
+      </div>
+    </ChartContext.Provider>
+  )
+}
+
+export const useChartContext = () => useContext(ChartContext)
+
+export default Chart
+
